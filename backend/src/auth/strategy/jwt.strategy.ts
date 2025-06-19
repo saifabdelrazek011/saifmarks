3733a6b2d5a9aaa895860bd899963132a5e91dc3
Saif Abdelrazek <saif@saifabdelrazek.com>
2025-06-19 22:54:36 +0300

    feat: implement JWT strategy for authentication with user validation

---
+++ backend/src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,35 @@
+import { ConfigService } from '@nestjs/config';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { JWTPayloadType } from 'src/types';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
+  constructor(
+    private config: ConfigService,
+    private prisma: PrismaService,
+  ) {
+    const jwtSecret = config.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
+      throw new Error('JWT_SECRET is not defined in the configuration');
+    }
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+    });
+  }
+
+  async validate(payload: JWTPayloadType) {
+    const user = await this.prisma.user.findUnique({
+      where: { id: payload.sub },
+    });
+    if (!user) {
+      throw new Error('User not found');
+    }
+    const { hashedPassword, ...userWithoutPassword } = user;
+
+    return userWithoutPassword;
+  }
+}
